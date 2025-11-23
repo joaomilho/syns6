d45cf53c323d9024b3b3875c5d6fd98c79ee0a50
Yo Lulkin <juanmaiz@gmail.com>
2025-11-23 20:00:36 +0100

    Improve viewer menu and footer

---
+++ src/components/Syns6Logo.tsx
@@ -0,0 +1,14 @@
+"use client";
+
+import styles from "./Syns6Logo.module.css";
+
+export default function Syns6Logo() {
+  return (
+    <div className={styles.logoContainer}>
+      <span className={styles.logoRed}>syns6</span>
+      <span className={styles.logoBlue}>syns6</span>
+      <span className={styles.logoWhite}>syns6</span>
+    </div>
+  );
+}
+
