f2128d50e8c355235236deddfe645adac1e51489
Yo Lulkin <juanmaiz@gmail.com>
2025-11-27 23:07:57 +0100

    Fix build

--- scripts/test-stripe-webhook.ts
+++ scripts/test-stripe-webhook.ts
@@ -20,7 +20,7 @@ if (!secretKey) {
 }
 
 const stripe = new Stripe(secretKey, {
-  apiVersion: '2024-11-20.acacia',
+  apiVersion: '2025-02-24.acacia',
 });
 
 async function testStripeConnection() {
