e7aabaef0dced1ef463fd65046afec0beabeb3a6
Yo Lulkin <juanmaiz@gmail.com>
2025-11-28 12:40:35 +0100

    Improve SEO

--- next-sitemap.config.js
+++ next-sitemap.config.js
@@ -36,5 +36,21 @@ module.exports = {
       lastmod: new Date().toISOString(),
     };
   },
+  additionalPaths: async (config) => {
+    const externalPages = [
+      'https://www.producthunt.com/products/syns6?launch=syns6',
+      'https://trylaunch.ai/launch/syns6',
+      'https://www.instagram.com/_syns6_/',
+      'https://www.tiktok.com/@_syns6_',
+      'https://x.com/_syns6_',
+    ];
+
+    return externalPages.map((url) => ({
+      loc: url,
+      changefreq: 'weekly',
+      priority: 0.5,
+      lastmod: new Date().toISOString(),
+    }));
+  },
 };
 
