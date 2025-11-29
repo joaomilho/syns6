74b2005cd2df9f549d827c345467c9a161dc1ec0
Yo Lulkin <juanmaiz@gmail.com>
2025-11-29 17:57:29 +0100

    Adds kaleidoscope

--- src/components/PsychedelicScene.tsx
+++ src/components/PsychedelicScene.tsx
@@ -5,29 +5,12 @@
  * Scene content only (no Canvas wrapper) for unified canvas
  */
 
-import { OrbitControls } from "@react-three/drei";
-import { PsychedelicBlob, KaleidoscopePlanes, LiquidParticles } from "./PsychedelicVisualization";
+import { KaleidoscopeShader } from "./PsychedelicVisualization";
 
-export default function PsychedelicScene({ micData }: { micData?: any }) {
+export default function PsychedelicScene({ micData, albumArt }: { micData?: any; albumArt?: string }) {
   return (
     <>
-      <ambientLight intensity={0.4} />
-      <pointLight position={[10, 10, 10]} intensity={1.5} color="#ff00ff" />
-      <pointLight position={[-10, -10, -10]} intensity={1} color="#00ffff" />
-      <pointLight position={[0, 10, -10]} intensity={1} color="#ffff00" />
-
-      <PsychedelicBlob audioFeatures={null} />
-      <KaleidoscopePlanes audioFeatures={null} />
-      <LiquidParticles audioFeatures={null} />
-
-      <OrbitControls
-        enableZoom={true}
-        enablePan={false}
-        minDistance={15}
-        maxDistance={50}
-        autoRotate={false}
-        autoRotateSpeed={0}
-      />
+      <KaleidoscopeShader audioFeatures={null} syncedData={null} micData={micData} albumArt={albumArt} />
     </>
   );
 }
